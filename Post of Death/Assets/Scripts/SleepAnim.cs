36afbe16803d7de44e306a4da294528773871e29
newtechnology <abhijeetzala81@gmail.com>
2015-08-13 23:35:00 +0530

    NT

---
+++ Post of Death/Assets/Scripts/SleepAnim.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SleepAnim : MonoBehaviour 
+{
+
+	// Use this for initialization
+	BedFunctions BedFunc;
+	Animation AnimComponent;
+
+	void Start () 
+	{
+		BedFunc = FindObjectOfType<BedFunc> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (BedFunc.NearBed && Keys.SecondaryActionKey.pressed) 
+		{
+			AnimComponent.Play("SleepAnim");
+		}
+	}
+}
