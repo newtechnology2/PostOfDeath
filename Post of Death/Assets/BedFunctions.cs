03d457a4cd91221e6e4b59a4004cbfe56ce8b208
IYP-Programer-Yeah <hgsilvermann@yahoo.com>
2015-08-13 08:44:23 +0430

    bed keys

---
+++ Post of Death/Assets/BedFunctions.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BedFunctions : MonoBehaviour {
+	public Transform camera;
+	public Transform RigidBody;
+	public Transform bed;
+	public Transform house;
+	public Text KeyMessage;
+	public float distane;
+
+	public static bool NearBed;
+
+	// Use this for initialization
+	void Start () {
+		NearBed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distane = (house.localPosition + camera.localPosition - bed.localPosition - RigidBody.localPosition).magnitude;
+		if ( distane< 2) {
+			NearBed = true;
+			KeyMessage.text = "Press ";
+			KeyMessage.text = KeyMessage.text + Keys.PrimaryActionKey.Key.ToString ();
+			KeyMessage.text = KeyMessage.text + " to sleep" + '\n' + "Press ";
+			KeyMessage.text = KeyMessage.text + Keys.SecondaryActionKey.Key.ToString ();
+			KeyMessage.text = KeyMessage.text + " to save game";
+		} else {
+			NearBed = false;
+			KeyMessage.text="";
+		}
+	}
+}
---
+++ Post of Death/Assets/Keys.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keys : MonoBehaviour {
+
+	public class PrimaryActionKey
+	{
+		public static KeyCode Key;
+		public static bool pressed;
+		public static string KeyName;
+	}
+	public class SecondaryActionKey
+	{
+		public static KeyCode Key;
+		public static bool pressed;
+		public static string KeyName;
+	}
+
+	// Use this for initialization
+	void Start () {
+		PrimaryActionKey.Key = KeyCode.F;
+		PrimaryActionKey.KeyName = "Primery Action";
+
+		SecondaryActionKey.Key = KeyCode.H;
+		SecondaryActionKey.KeyName = "Secondary Action";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PrimaryActionKey.pressed = Input.GetKeyDown (PrimaryActionKey.Key);
+		SecondaryActionKey.pressed = Input.GetKeyDown (SecondaryActionKey.Key);
+	}
+}
