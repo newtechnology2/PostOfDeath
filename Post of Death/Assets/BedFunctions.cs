1c645a2db40ac5dcdaf4ef66b412df7032a6ab94
IYP-Programer-Yeah <hgsilvermann@yahoo.com>
2015-08-13 22:30:26 +0430

    some stuff

--- Post of Death/Assets/BedFunctions.cs
+++ Post of Death/Assets/BedFunctions.cs
@@ -11,7 +11,7 @@ public class BedFunctions : MonoBehaviour {
 	public Vector3 EffectingDirection;
 	public float HalfEffectingAngleCos2;
 
-	public static bool NearBed;
+	public bool NearBed;
 
 	// Use this for initialization
 	void Start () {
