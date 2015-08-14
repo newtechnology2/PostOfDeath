8558c51f9b2eb529a152171f1dc792567b88d277
newtechnology <abhijeetzala81@gmail.com>
2015-08-14 21:19:47 +0530

    Added ghost attack animations

---
+++ Post of Death/Assets/Scripts/OnGhostAttack.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+
+public class OnGhostAttack : MonoBehaviour 
+{
+
+    Ghost GhostScript;
+    Animation AnimComponent;
+    RigidbodyFirstPersonController PlayerController;
+
+    bool PlayedFallAnim = false;
+    bool StoodUp = false;
+
+    float TimeElapsed = 0;
+
+	void Start () 
+    {
+        GhostScript = FindObjectOfType<Ghost>();
+        AnimComponent = GetComponent<Animation>();
+        PlayerController = GetComponent<RigidbodyFirstPersonController>();
+
+
+        PlayedFallAnim = false;
+        StoodUp = false;
+
+        TimeElapsed = 0;
+	}
+	
+	
+	void Update () 
+    {
+        if (GhostScript.GhostAttacked && !AnimComponent.IsPlaying("ThrowAnim") && !PlayedFallAnim)
+        {
+
+            PlayerController.enabled = false;
+
+            AnimComponent.Play("ThrowAnim");
+
+            PlayedFallAnim = true;
+
+            StoodUp = false;
+        }
+
+        if (PlayedFallAnim && !AnimComponent.IsPlaying("ThrowAnim") && !AnimComponent.IsPlaying("StandUpAnim"))
+        {
+            TimeElapsed += Time.deltaTime;
+
+            if (TimeElapsed >= 3.0f)
+            {
+                AnimComponent.Play("StandUpAnim");
+
+                PlayedFallAnim = false;
+
+                StoodUp = true;
+
+                TimeElapsed = 0;
+            }
+        }
+
+        if (StoodUp)
+            PlayerController.enabled = true;
+	}
+}
