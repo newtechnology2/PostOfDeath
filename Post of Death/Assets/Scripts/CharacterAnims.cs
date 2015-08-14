b0d1bcf7824f3b94357bc5a078e905b3868acbc2
newtechnology <abhijeetzala81@gmail.com>
2015-08-15 01:46:42 +0530

    Character anims

---
+++ Post of Death/Assets/Scripts/CharacterAnims.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnims : MonoBehaviour 
+{
+
+    Animation AnimComponent;
+
+    bool PlayWalk = false;
+    bool PlayJump = false;
+
+    void Start ()
+    {
+        AnimComponent = FindObjectOfType<Animation>();
+    }
+
+	void Update () 
+    {
+        AnimComponent.CrossFade("idle");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+           AnimComponent.CrossFade("walk");
+        }
+
+ //       if (Input.GetKey(KeyCode.S))
+  //      {
+  //          AnimComponent["walk"].speed = -1;
+  //   
+  //          AnimComponent.CrossFade("walk");
+  //      }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayJump = true;
+           
+        } else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            PlayJump = false;
+        }
+
+
+        if (PlayJump)
+        {
+            if (AnimComponent.IsPlaying("jump"))
+            {
+                if (AnimComponent["jump"].normalizedTime >= 0.99f)
+                {
+                    PlayJump = false;
+                    return;
+                }
+            }
+
+            AnimComponent.CrossFade("jump");
+        }
+	}
+}
