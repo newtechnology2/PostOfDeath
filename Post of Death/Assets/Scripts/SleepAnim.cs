493302e80fcaf16f204b641913701fcef9aa16c0
newtechnology <abhijeetzala81@gmail.com>
2015-08-14 13:01:03 +0530

    Terrain fixes

--- Post of Death/Assets/Scripts/SleepAnim.cs
+++ Post of Death/Assets/Scripts/SleepAnim.cs
@@ -10,28 +10,55 @@ public class SleepAnim : MonoBehaviour
 	Animation AnimComponent;
 	RigidbodyFirstPersonController PlayerControl;
 
+    bool Played = false;
+
+    Transform MainCamera;
+
 	void Start () 
 	{
 		B = FindObjectOfType<BedFunctions> ();
 		AnimComponent = GetComponent<Animation> ();
 		PlayerControl = GetComponent<RigidbodyFirstPersonController> ();
 
+        Played = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-        if (!AnimComponent.IsPlaying("SleepAnim"))
-        {
-            PlayerControl.enabled = true;
-        }
+        //if (!AnimComponent.IsPlaying("SleepAnim") && Played)
+      //  {
+            //PlayerControl.enabled = true;
+      //  }
 
-		if (B.NearBed && Keys.PrimaryActionKey.pressed) 
+        if (B.NearBed && Keys.PrimaryActionKey.pressed && !Played && !AnimComponent.IsPlaying("SleepAnim")) 
 		{
+            Debug.Log("Play Sleep Anim");
+
+            AnimComponent["SleepAnim"].speed = +1;
+           // AnimComponent["SleepAnim"].time = AnimComponent["SleepAnim"].length;
+      
 			AnimComponent.Play("SleepAnim");
         
             PlayerControl.enabled = false;
+
+            Played = true;
 		}
+       
+        if (Keys.PrimaryActionKey.pressed && Played && !AnimComponent.IsPlaying("SleepAnim")) 
+        {
+            Debug.Log("Reverse Play Sleep Anim");
+
+            AnimComponent["SleepAnim"].speed = -1;
+            AnimComponent["SleepAnim"].time = AnimComponent["SleepAnim"].length;
+
+            AnimComponent.Play("SleepAnim");
+
+            PlayerControl.enabled = true;
+
+            Played = false;
+        
+        }
 
 
 	}
