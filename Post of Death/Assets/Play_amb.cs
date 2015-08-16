d9e2e861a181a277fd9ffff344c4618263405275
geartree <adrianarmeanu@gmail.com>
2015-08-16 12:48:30 -0400

    Fixing some soundies!
    asdasdsfdasf sadf

--- Post of Death/Assets/Play_amb.cs
+++ Post of Death/Assets/Play_amb.cs
@@ -9,7 +9,19 @@ public class Play_amb : MonoBehaviour {
 
 	public void play_audio()
 	{
-
+		if (Clock.GetTime ().Hours >= 8 && Clock.GetTime ().Hours <= 20) {
+			if(Day.isPlaying == false) 
+			{
+				Night.Stop();
+				Day.Play();
+			}
+		}else if (Clock.GetTime ().Hours > 20 || Clock.GetTime ().Hours < 8) {
+			if(Night.isPlaying == false) 
+			{
+				Day.Stop();
+				Night.Play();
+			}
+		}
 	}
 
 	void Start () {
@@ -18,13 +30,8 @@ public class Play_amb : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Clock.GetTime ().Hours >= 8 || Clock.GetTime ().Hours <= 20) {
-			if(Night.isPlaying == false)	
-				Night.Play();
-		}else if (Clock.GetTime ().Hours >= 20 || Clock.GetTime ().Hours <= 8) {
-			if(Night.isPlaying == false)	
-				Day.Play();
+		play_audio ();
 		}
 	
-	}
 }
+
