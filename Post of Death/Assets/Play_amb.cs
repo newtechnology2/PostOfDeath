e6cbc6c747415ccf122bedf4d3ace7af5b45ec80
geartree <adrianarmeanu@gmail.com>
2015-08-15 23:05:38 -0400

    The sound chanegs
    ssss

---
+++ Post of Death/Assets/Play_amb.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play_amb : MonoBehaviour {
+
+	// Use this for initialization
+	public  AudioSource Night;
+	public  AudioSource Day;
+
+	public void play_audio()
+	{
+
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Clock.GetTime ().Hours >= 8 || Clock.GetTime ().Hours <= 20) {
+			if(Night.isPlaying == false)	
+				Night.Play();
+		}else if (Clock.GetTime ().Hours >= 20 || Clock.GetTime ().Hours <= 8) {
+			if(Night.isPlaying == false)	
+				Day.Play();
+		}
+	
+	}
+}
